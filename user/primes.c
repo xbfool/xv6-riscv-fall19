409da91ddbd15ed0a7f01a064489fd5c7ee82fe5
xbfool <xbfool@gmail.com>
2020-03-22 23:06:49 +0800

    fix fd bug

--- user/primes.c
+++ user/primes.c
@@ -4,35 +4,33 @@
 
 int prime(int i)
 {
-    int p[10];
+    int p[2];
 //    char buf[5];
 //    int b[1];
 //    int tmp;
     pipe(p);
-    printf("\t\t\tpipe: %d %d\n", p[0], p[1]);
+    //printf("\t\t\tpipe: %d %d\n", p[0], p[1]);
     int num;
  //   buf[4] = 0;
     //printf("new num %d\n", i);
-    sleep(10);
+    //sleep(10);
     int len;
     if (fork() == 0)
     {
+        close(0);
+        dup(p[0]);
+        close(p[0]);
         int fd;
         printf("is prime %d\n", i);
-        close(0);
-        close(1);
-        dup(p[0]);
-        dup(p[1]);
-        close(p[0]);
-        close(p[1]);
         fd = -1;
+        close(p[1]);
         //close(0);
-        while ((len = read(p[0], &num, sizeof(num))))
+        while ((len = read(0, &num, sizeof(num))))
         {
             if(len == -1){
                 continue;
             }
-            printf("pid: %d read %d\n", getpid(), num);
+            //printf("pid: %d read %d\n", getpid(), num);
             if (num == 0)
             {
                 if (fd != -1)
@@ -51,12 +49,12 @@ int prime(int i)
                 if (fd == -1)
                 {
                     fd = prime(num);
-                    printf("pid: %d prime %d\n", getpid(), num);
+                    //printf("pid: %d prime %d\n", getpid(), num);
                 }
                 else
                 {
                     //printf("write %d\n", num);
-                    printf("pid: %d write %d\n", getpid(), num);
+                    //printf("pid: %d write %d\n", getpid(), num);
                     write(fd, (&num), sizeof(num));
                 }
             }
@@ -74,9 +72,9 @@ int main(int argc, char *argv[])
     int fd;
     int i;
     fd = prime(2);;
-    for (i = 3; i < 35; i++)
+    for (i = 3; i < 100; i++)
     {
-        printf("num: %d\n", i);
+        //printf("num: %d\n", i);
         write(fd, (&i), sizeof(i));
     }
     i = 0;
